f60adb93e607875da6ab18b6df773f078889a2a3
hl1itj <com410-1@com410-1-PC>
2016-11-15 13:49:12 +0900

    client 담당

---
+++ Client/src/kr/sc/sunmoon/AudioClient.java
@@ -0,0 +1,16 @@
+package kr.sc.sunmoon;
+
+import java.io.*;
+import java.net.*;
+import javax.sound.sampled.*;
+
+
+public class AudioClient {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		if(args.length > 0) {
+			File soundFile = AudioUtil.getSoundFile(args[0];)
+		}
+	}
+
+}
