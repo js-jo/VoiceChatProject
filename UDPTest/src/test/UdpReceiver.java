e1325e4bf707c1a31ac822898d63f8bb9408b708
chojinsung <cjs97111@gmail.com>
2016-11-21 18:22:28 +0900

    실시간성 Test ver2

--- UDPTest/src/test/UdpReceiver.java
+++ UDPTest/src/test/UdpReceiver.java
@@ -7,6 +7,7 @@ import java.net.SocketException;
 
 public class UdpReceiver 
 {
+	 private boolean flag = true;
 	 final int port = 10000;
 	 DatagramSocket socket;
 	 public UdpReceiver()
@@ -17,13 +18,20 @@ public class UdpReceiver
 			
 			DatagramPacket p = new DatagramPacket(msg, msg.length);
 			System.out.println("수신 대기중 ... ");
-			socket.receive(p);
-			System.out.println("수신 완료");
-			System.out.println("ip : " + p.getAddress());
-			System.out.println("수신 내용  : " + new String(msg));
+			while(flag)
+			{
+				Thread.sleep(1000);
+				socket.receive(p);
+				System.out.println("수신 완료");
+				System.out.println("ip : " + p.getAddress());
+				System.out.println("수신 내용  : " + new String(msg));
+			}
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
