5b511b7d6e6f83f9e83ee197129005e5ca2e32a8
jungyouna <jyn1996@naver.com>
2016-11-22 12:49:26 +0900

    데이터 전송 코드 수정

--- Client/src/project/oss/sunmoon/AudioClient.java
+++ Client/src/project/oss/sunmoon/AudioClient.java
@@ -1,37 +1,43 @@
 package project.oss.sunmoon;
-
+ 
 import java.io.*; //클라이언트, 서버를 사용하는 소켓 선언
 import java.net.*; //네트워크 소켓선언
+import javax.sound.sampled.*; //사운드 선언
  
 public class AudioClient{
+	
 	private static final int bufferSize = 0;
-
+	byte buffer[] = new byte[bufferSize];
+	@SuppressWarnings("unused")
+	private AudioFormat getFormat;
+	
 	public static void main(String[] args) {
-
+		@SuppressWarnings("unused")
+		AudioClient audio = new AudioClient();
+	}
+	
+	public AudioClient(){
+		boolean stopaudioCapture = false; //오디오 멈춤
+		
 		try{
 			DatagramSocket socket = new DatagramSocket(null);
 			//데이터를 주고받기 위해서 udp 소켓 연결
-			InetSocketAddress ip = new InetSocketAddress("220.69.166.100",6800);
-			//연결을 원하는 소켓 주소 작성
-			byte[] send = new byte[1024];
-			byte[] receive = new byte[1024];
-			//보내고 받을 데이터 바이트 배열로 변환
-
-			socket.bind(ip); //연결
-			System.out.println("연결을 완료하였습니다.");
-			
-			DatagramPacket sendpacket = new DatagramPacket(send, send.length, ip);
-			//각 정보를 담고 전송
-			socket.send(sendpacket); //패킷 전송
-			
-			DatagramPacket receivepacket = new DatagramPacket(receive, receive.length);
-			//각 정보를 수신
-			socket.receive(receivepacket); //패킷 수신
-		}
-		
-		catch(IOException e){ //예외
+			InetAddress ip = InetAddress.getByName("192.168.219.104");
+			DatagramPacket sendpacket = new DatagramPacket(buffer, buffer.length, ip, 7000);
+			//socket.send(socket);
+		} catch(IOException e){ //예외
 			e.printStackTrace(); //예외 상황 해결
 			}
-		}
+	}
+	
+	@SuppressWarnings("unused")
+	private AudioFormat getFormat() {
+		float sampleRate = 8000;
+		int sampleSizeInBits = 8;
+		int channels = 1;
+		boolean signed = true;
+		boolean bigEndian = true;
+		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+	}
 }
-
+ 
