2b7edb920eb7f438b6f1cb1b452a3b8eb25bc84e
jungyouna <jyn1996@naver.com>
2016-11-29 14:12:38 +0900

    클라이언트 코드 재수정 완료

--- TotalProject/src/project/oss/sunmoon/udp/AudioClient.java
+++ TotalProject/src/project/oss/sunmoon/udp/AudioClient.java
@@ -1,11 +1,13 @@
 package project.oss.sunmoon.udp;
 
-import java.io.ByteArrayInputStream;
 //바이트 데이터 관련 소켓 선언
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+
 //클라이언트, 서버를 사용하는 소켓 선언
 import java.io.IOException;
 import java.io.InputStream;
+
 //네트워크 소켓 선언
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,18 +18,17 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
+
 //오디오 출력 소켓 선언
 import javax.sound.sampled.TargetDataLine;
 
 public class AudioClient{
 
 	boolean stopaudioCapture = false; //오디오 멈춤
-	//private static final int bufferSize = 10000; //버퍼사이즈 지정
-	//@SuppressWarnings("unused")
 	private AudioFormat getFormat = getFormat(); //오디오 재생형식 지정
 	ByteArrayOutputStream data; //바이트 데이터
 	TargetDataLine targetDataLine; //오디오 출력
-	private boolean flag = false;
+	private boolean flag = false; //연결
 	private int port;
 
 	public void setPort(int port) {
@@ -42,7 +43,6 @@ public class AudioClient{
 		this.flag = flag;
 	}
 
-	@SuppressWarnings("unused")
 	private AudioFormat getFormat() {
 		float sampleRate = 8000;
 		int sampleSizeInBits = 8;
@@ -52,21 +52,7 @@ public class AudioClient{
 		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
 	}
 
-	//	public static void main(String[] args) {
-	//		@SuppressWarnings("unused")
-	//		DatagramSocket socket = null;
-	//	}
-
 	public AudioClient(String ipAddress, int port){
-		//		DatagramSocket socket = null;
-		//		data = new ByteArrayOutputStream(); //바이트 데이터로 저장
-		//		//InputStream input = new ByteArrayInputStream(audio); //바이트 데이터를 읽음
-		//		stopaudioCapture = false; //오디오 멈춤
-		//		setPort(port);
-		//		final AudioFormat format = getFormat();
-		//		int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
-		//		byte buffer[] = new byte[bufferSize];
-		//		
 
 		try {		
 			final AudioFormat format = getFormat();
@@ -83,17 +69,14 @@ public class AudioClient{
 					try{
 						int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
 						byte buffer[] = new byte[bufferSize];
-						DatagramSocket socket = new DatagramSocket();
+						DatagramSocket socket = new DatagramSocket(); //데이터 소켓 생성
 						InetAddress ip = InetAddress.getByName(ipAddress); //전송할 ip번호
 						while(true)
-						{						//						
-							//byte audio[] = out.getByteArray(); //전송할 음성 데이터
+						{							
 							System.out.println("여기서 무한루프 클라이언트");
 							while(!stopaudioCapture){ //오디오 멈춤이 아니면
 								int cnt = line.read(buffer, 0, buffer.length); //버퍼 길이 읽음
 								if(cnt > 0){ //버퍼 길이가 0보다 크면
-									//String str = "야이 멍청아";
-									//byte[] buffer = str.getBytes();
 									DatagramPacket sendpacket = new DatagramPacket(buffer, buffer.length, ip, port);
 									//주고 받을 데이터와 관련 클래스
 									socket.send(sendpacket); 
@@ -104,8 +87,8 @@ public class AudioClient{
 							//소켓 종료
 						}
 
-					}catch(IOException e){ //예외
-						e.printStackTrace(); //예외 상황 해결
+					}catch(IOException e){ //예외 처리
+						e.printStackTrace();
 					}
 				}
 			};
